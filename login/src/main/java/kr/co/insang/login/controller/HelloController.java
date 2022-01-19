1464ba34466936fc9156acced602d505871cf879
insang <developer_91@naver.com>
2022-01-19 18:35:20 +0900

    inflix project base. login server with docker 빌드 되도록 환경 설정 완료.
    TODO
     1. login server 개발
     2. 다른서버 추가(jwt, streaming, main ...)

---
+++ login/src/main/java/kr/co/insang/login/controller/HelloController.java
@@ -0,0 +1,13 @@
+package kr.co.insang.login.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "This server will be Login Server.";
+    }
+
+}
---
+++ login/src/test/java/kr/co/insang/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.insang.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
