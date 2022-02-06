07dcc7968d7717d551267da1ce974d63428cc073
insang <developer_91@naver.com>
2022-02-07 00:58:43 +0900

    Frontend(vue기반)으로 만드는중에 API Gateway가 먼저 개발되어야해서 개발중. SCG 이용해서 개발중인데 추후 Auth Service와 합칠수도 있음.

---
+++ gateway/src/main/java/kr/co/insang/gateway/filters/LoginFilter.java
@@ -0,0 +1,39 @@
+package kr.co.insang.gateway.filters;
+
+import lombok.Data;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoginFilter extends AbstractGatewayFilterFactory<LoginFilter.Config> {
+    private static final Logger logger = LogManager.getLogger(LoginFilter.class);
+    public LoginFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            logger.info("LoginFilter baseMessage>>>>>>" + config.getBaseMessage());
+            if (config.isPreLogger()) {
+                logger.info("LoginFilter Start>>>>>>" + exchange.getRequest());
+            }
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                if (config.isPostLogger()) {
+                    logger.info("LoginFilter End>>>>>>" + exchange.getResponse());
+                }
+            }));
+        });
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
