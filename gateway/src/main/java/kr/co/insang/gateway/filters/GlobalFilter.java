3d830a919f2093ecaa6da067d605e25000cb6219
insang <developer_91@naver.com>
2022-02-10 21:50:41 +0900

    간단하게 라우팅 테스트 완료. TODO: 1. Merge with Autho Service 2. 프론트엔드 로그인 기능 개발하면서 토큰주고 검증하는 로직 개발.

--- gateway/src/main/java/kr/co/insang/gateway/filters/GlobalFilter.java
+++ gateway/src/main/java/kr/co/insang/gateway/filters/GlobalFilter.java
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -21,6 +23,12 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
             logger.info("GlobalFilter baseMessage>>>>>>" + config.getBaseMessage());
             if (config.isPreLogger()) {
                 logger.info("GlobalFilter Start>>>>>>" + exchange.getRequest());
+
+
+                //홈화면, 회원가입, 아이디주복확인을 제외한 모든 요청은 Http Header에 Auth 정보가없으면 보내지않는다.
+                //로직 추가 예정.
+                //ServerHttpRequest httpRequest = exchange.getRequest();
+                //HttpHeaders httpHeader = httpRequest.getHeaders();
             }
             return chain.filter(exchange).then(Mono.fromRunnable(()->{
                 if (config.isPostLogger()) {
